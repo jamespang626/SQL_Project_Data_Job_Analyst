39986bf2e48ad0cea9b7258a1a0b52f0fecae3fc
jamespang626 <gamingshark81@gmail.com>
2026-06-14 18:52:27 -0800

    upload sql files

---
+++ project_sql/5_optimal_skills.sql
@@ -0,0 +1,48 @@
+WITH skills_demand AS (
+    SELECT 
+        skills_dim.skill_id,
+        skills_dim.skills,
+        COUNT(skills_job_dim.job_id) AS demand_count
+    FROM job_postings_fact
+    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE
+        job_title_short = 'Data Analyst' AND
+        (job_location LIKE '%San Jose%' OR 
+        job_location LIKE '%San Francisco%') AND
+        salary_year_avg IS NOT NULL
+    GROUP BY
+        skills_dim.skill_id
+)
+, 
+average_salary AS (
+    SELECT 
+        skills_dim.skill_id,
+        skills_dim.skills,
+        ROUND(AVG(salary_year_avg), 0) AS average_salary
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Data Analyst' AND
+    (job_location LIKE '%San Jose%' OR 
+    job_location LIKE '%San Francisco%') AND
+    salary_year_avg IS NOT NULL
+GROUP BY
+    skills_dim.skill_id
+)
+
+SELECT
+    skills_demand.skill_id,
+    skills_demand.skills,
+    demand_count,
+    average_salary
+FROM
+    skills_demand
+INNER JOIN average_salary ON skills_demand.skill_id = average_salary.skill_id
+WHERE 
+    demand_count > 10
+ORDER BY
+    demand_count DESC,
+    average_salary DESC
+LIMIT 25;
